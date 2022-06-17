b49dc4c09bbd70100f06d4f90492f9a0a7ca0451
claudiojuniops <claudiojunio@outlook.com>
2022-06-17 15:40:50 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -2,16 +2,27 @@
 
 require_once("config.php");
 
-$root = new usuario();
+//carrga um usuário
+//$root = new usuario();
+//$root->loadById(1);
+//echo $root;
 
-$root->loadById(1);
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $root;
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("Ge");
+//echo json_encode($search);
 
+//Carrega um usuário usando o login e a senha
+$usuario = new usuario ();
+$usuario->login("Tata", "9298");
+echo $usuario;
+
+// teste começo
 //$sql = new sql();
-
 //$usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
 //echo json_encode($usuarios);
 
  ?>
